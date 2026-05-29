d71c4fd0ede39d440727b5dee8fe4a932d350328
Dellie8 <166549834+Dellie8@users.noreply.github.com>
2026-05-29 23:00:17 +0300

    исключили точки

--- Homeworks/21.03/LQ/LQ/LQ.cs
+++ Homeworks/21.03/LQ/LQ/LQ.cs
@@ -61,14 +61,14 @@ public static class LQ2_PointExtensions
         int[] dx = { -1, -1, -1, 0, 0, 1, 1, 1 };
         int[] dy = { -1, 0, 1, -1, 1, -1, 0, 1 };
         
-        var neighbors = points
-            .SelectMany(p => Enumerable.Range(0, 8)
-                .Select(i => new Point(p.X + dx[i], p.Y + dy[i])));
+        var sourcePoints = new HashSet<Point>(points);
         
         return points
-            .Concat(neighbors)
-            .Distinct()
-            .ToHashSet();
+        .SelectMany(p => Enumerable.Range(0, 8)
+            .Select(i => new Point(p.X + dx[i], p.Y + dy[i])))
+        .Where(p => !sourcePoints.Contains(p)) 
+        .Distinct()
+        .ToHashSet();
     }
 }
 
