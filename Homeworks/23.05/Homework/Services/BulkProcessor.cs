097b91ffe7b21fba12742c16e8861bf20210a6e3
Dellie8 <166549834+Dellie8@users.noreply.github.com>
2026-05-30 00:47:26 +0300

    Add files via upload

---
+++ Homeworks/23.05/Homework/Services/BulkProcessor.cs
@@ -0,0 +1,48 @@
+﻿using ReflectionClass.Homework.DTOs;
+using ReflectionClass.Homework.Utils.MyMapper;
+using ReflectionClass.Homework.Utils.Validators.Abstraction;
+
+namespace ReflectionClass.Homework.Services;
+
+public class BulkProcessor
+{
+    private readonly IValidator _validator;
+    private readonly IMapper _mapper;
+    
+    // Зависимости внедряются через конструктор (DI)
+    public BulkProcessor(IValidator validator, IMapper mapper)
+    {
+        _validator = validator;
+        _mapper = mapper;
+    }
+    
+    public List<UserDto> ProcessParallel(List<object> rawItems, out List<string> allErrors)
+    {
+        var validDtos = new List<UserDto>();
+        var errors = new List<string>();
+
+        // TODO: Распараллелить через Parallel.ForEach
+        Parallel.ForEach(rawItems, item =>
+        {
+            var validationErrors = _validator.Validate(item);
+
+            if (validationErrors.Any())
+            {
+                foreach (var error in validationErrors)
+                    errors.Add(error);
+            }
+            else
+            {
+                var dto = _mapper.Map<UserDto>(item);
+                validDtos.Add(dto);
+            }
+        });
+
+        allErrors = errors.ToList();
+        return validDtos.ToList();
+        });
+
+        allErrors = errors;
+        return validDtos;
+    }
+}
---
+++ Homeworks/23.05/Homework/Utils/Validators/Implementation/UniversalValidator.cs
@@ -0,0 +1,87 @@
+﻿using ReflectionClass.Homework.Utils.Validators.Abstraction;
+
+namespace ReflectionClass.Homework.Utils.Validators.Implementation;
+
+public class UniversalValidator : IValidator
+{
+        /// <summary>
+        /// Универсальный метод, который валидирует ВООБЩЕ любой объект на основе его атрибутов.
+        /// </summary>
+        public bool Validate(object? obj, out List<string> errors)
+        {
+            errors = new List<string>();
+            
+            // TODO: Проверить на null
+            if (obj == null)
+            {
+                errors.Add("Object cannot be null");
+                return false;
+            }
+            
+            // TODO: ШАГ 1. Получить тип объекта 
+            Type objectType = obj.GetType();
+            
+            // TODO: ШАГ 2. Извлечь все свойства
+            PropertyInfo[] properties = objectType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            
+            // TODO: ШАГ 3. Получать все значения свойств у ТЕКУЩЕГО экземпляра
+            foreach (var property in properties)
+            {
+                object? value = property.GetValue(obj);
+
+            // TODO: ШАГ 3.1 Проверять, обвешано ли свойство атрибутом MyRequired
+                if (Attribute.IsDefined(property, typeof(MyRequiredAttribute)))
+                {
+                    if (value == null)
+                    {
+                        errors.Add($"Property '{property.Name}' is required but is null");
+                    }
+                    else if (value is string str && string.IsNullOrWhiteSpace(str))
+                    {
+                        errors.Add($"Property '{property.Name}' is required but is empty or whitespace");
+                    }
+                }
+            // TODO: ШАГ 3.2 Проверять, есть ли атрибут MyRange
+            var rangeAttr = property.GetCustomAttribute<MyRangeAttribute>();
+            if (rangeAttr != null)
+            {
+                if (value is IComparable comparable)
+                {
+                    try
+                    {
+                        if (comparable.CompareTo(rangeAttr.Min) < 0 || comparable.CompareTo(rangeAttr.Max) > 0)
+                        {
+                            errors.Add($"Property '{property.Name}' value '{value}' is not in range [{rangeAttr.Min}, {rangeAttr.Max}]");
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        errors.Add($"Property '{property.Name}' cannot be compared with range values");
+                    }
+                }
+                else if (value != null)
+                {
+                    errors.Add($"Property '{property.Name}' is not comparable (cannot apply range validation)");
+                }
+            }
+            }
+        
+            return errors.Count == 0;
+        }
+}
+
+[AttributeUsage(AttributeTargets.Property)]
+public class MyRequiredAttribute : Attribute { }
+
+[AttributeUsage(AttributeTargets.Property)]
+public class MyRangeAttribute : Attribute
+{
+    public object Min { get; }
+    public object Max { get; }
+    
+    public MyRangeAttribute(object min, object max)
+    {
+        Min = min;
+        Max = max;
+    }
+}
